17629f5afd704ea3228cab0111c32f0aca647fc4
Pres654 <113102587+Pres654@users.noreply.github.com>
2025-05-22 09:30:42 -0500

    Add files via upload

---
+++ Random-Number-Gen.ps1
@@ -0,0 +1,144 @@
+function Write-AsciiArtBanner {
+    param (
+        [string]$Text
+    )
+    Write-Host "======================================================================" -ForegroundColor Cyan
+    $alignmentWidth = [int](($Host.UI.RawUI.WindowSize.Width + $Text.Length) / 2)
+    Write-Host ("{0,$alignmentWidth}" -f $Text) -ForegroundColor Yellow
+    Write-Host "======================================================================" -ForegroundColor Cyan
+    Write-Host ""
+}
+
+function Write-AsciiArtNumber {
+    param (
+        [int]$Number
+    )
+
+    $numberStr = $Number.ToString()
+    $asciiDigits = @{
+        '0' = @(
+            " 0000 ",
+            "00  00",
+            "00  00",
+            "00  00",
+            " 0000 "
+        )
+        '1' = @(
+            "1111  ",
+            "  11  ",
+            "  11  ",
+            "  11  ",
+            "111111"
+        )
+        '2' = @(
+            " 2222 ",
+            "22  22",
+            "   22 ",
+            "  22  ",
+            "222222"
+        )
+        '3' = @(
+            " 3333 ",
+            "33  33",
+            "   333",
+            "33  33",
+            " 3333 "
+        )
+        '4' = @(
+            "44  44",
+            "44  44",
+            "444444",
+            "    44",
+            "    44"
+        )
+        '5' = @(
+            "555555",
+            "55    ",
+            "55555 ",
+            "    55",
+            "55555 "
+        )
+        '6' = @(
+            " 6666 ",
+            "66    ",
+            "66666 ",
+            "66  66",
+            " 6666 "
+        )
+        '7' = @(
+            "777777",
+            "   77 ",
+            "  77  ",
+            " 77   ",
+            "77    "
+        )
+        '8' = @(
+            " 8888 ",
+            "88  88",
+            " 8888 ",
+            "88  88",
+            " 8888 "
+        )
+        '9' = @(
+            " 9999 ",
+            "99  99",
+            " 99999",
+            "    99",
+            " 9999 "
+        )
+    }
+    $outputLines = @("", "", "", "", "")
+    foreach ($char_digit in $numberStr.ToCharArray()) {
+        $charKey = $char_digit.ToString()
+        if ($asciiDigits.ContainsKey($charKey)) {
+            $digitArt = $asciiDigits[$charKey]
+            for ($i = 0; $i -lt $digitArt.Length; $i++) {
+                $outputLines[$i] += $digitArt[$i] + "  " 
+            }
+        } else {
+            for ($i = 0; $i -lt $outputLines.Length; $i++) {
+                $outputLines[$i] += "?????  "
+            }
+        }
+    }
+
+    Write-Host "Your random number is:" -ForegroundColor Green
+    foreach ($line in $outputLines) {
+        Write-Host $line -ForegroundColor Magenta
+    }
+    Write-Host ""
+}
+Clear-Host
+Write-AsciiArtBanner -Text "RANDOM NUMBER GENERATOR"
+$minNumber = $null
+$maxNumber = $null
+$minInput = $null
+$maxInput = $null
+while ($minNumber -eq $null -or -not ($minInput -match "^\d+$")) {
+    $minInput = Read-Host "Enter the first (minimum) whole number"
+    if ($minInput -match "^\d+$") {
+        $minNumber = [int]$minInput
+    } else {
+        Write-Host "Invalid input. Please enter a whole number." -ForegroundColor Red
+    }
+}
+while ($maxNumber -eq $null -or -not ($maxInput -match "^\d+$") -or $maxNumber -lt $minNumber) {
+    $maxInput = Read-Host "Enter the second (maximum) whole number (must be >= $minNumber)"
+    if ($maxInput -match "^\d+$") {
+        $tempMax = [int]$maxInput
+        if ($tempMax -ge $minNumber) {
+            $maxNumber = $tempMax
+        } else {
+            Write-Host "Maximum number must be greater than or equal to the minimum number ($minNumber)." -ForegroundColor Red
+            $maxInput = $null
+        }
+    } else {
+        Write-Host "Invalid input. Please enter a whole number." -ForegroundColor Red
+    }
+}
+$randomNumber = Get-Random -Minimum $minNumber -Maximum ($maxNumber + 1)
+Write-Host ""
+Write-AsciiArtNumber -Number $randomNumber
+
+Write-Host "======================================================================" -ForegroundColor Cyan
+Write-Host "Done!" -ForegroundColor Green
